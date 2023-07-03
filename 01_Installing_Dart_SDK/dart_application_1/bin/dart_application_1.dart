45109e85266495cfa14e9d8f9ae6c1c7173116f6
orgoone <orgo.geza@gmail.com>
2023-07-03 19:41:29 +0200

    Installed Dart SDK to my computer, and ran my 1st Hello World

---
+++ 01_Installing_Dart_SDK/dart_application_1/bin/dart_application_1.dart
@@ -0,0 +1,6 @@
+import 'package:dart_application_1/dart_application_1.dart' as dart_application_1;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_application_1.calculate()}!');
+  print('This is very similar to Javascript, hope I can learn it quickly!');
+}
