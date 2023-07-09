ee1310fc8ec6287e9361e952e55c72d2d2ae1d6c
orgoone <orgo.geza@gmail.com>
2023-07-09 12:34:32 +0200

    notes on if statements

---
+++ 03_If_Ternary_Switch/if_statement.dart
@@ -0,0 +1,21 @@
+void main() {
+
+/*Possible operators that we can use in Dart if:
+>
+<
+>=
+<=
+==
+*/
+
+int age = 18;
+
+if (age >= 18) {
+  print("Adult");
+} else {
+  print("Child");
+};
+
+
+
+}
