726bd6390f5fb8aa717767bc072ae7deac05f5cc
orgoone <orgo.geza@gmail.com>
2023-07-09 13:11:25 +0200

    notes on IF statement

--- 03_If_Ternary_Switch/if_statement.dart
+++ 03_If_Ternary_Switch/if_statement.dart
@@ -1,3 +1,5 @@
+bool isAllowed = false;
+
 void main() {
 
 /*Possible operators that we can use in Dart if:
@@ -6,16 +8,29 @@ void main() {
 >=
 <=
 ==
+!=
+&&
+||
 */
 
 int age = 18;
 
 if (age >= 18) {
   print("Adult");
+} else if (age <= 0) {
+  print("Unborn");
 } else {
   print("Child");
 };
 
+if (age < 18 && true) {
+  print("You shall not pass");
+}
 
+if (age > 18 || isAllowed) { 
+  print("You shall pass");
+}
+//note: if the isAllowed variable would have been declared within the function, the compiler would have thrown an error as it already knows it's value.
+//functions are compiled in a separate scope than the rest of the code.
 
 }
