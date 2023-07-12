93d5b3572322126ad4501008bf3159f6b6f02954
orgoone <orgo.geza@gmail.com>
2023-07-12 21:36:25 +0200

    While loop notes

---
+++ 04_Loops/while_loop.dart
@@ -0,0 +1,13 @@
+void main() {
+
+//while loops will run as long as the passed condition evaluates to true
+
+String greeting = "Hello there!";
+int i = 0;
+
+while (i < greeting.length){
+print(greeting[i]);
+i++;
+}
+
+}
